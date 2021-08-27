4e0f1a1d7c9304b84f9f70b178d1327621d72f54
rhenzol <54154565+rhenzol@users.noreply.github.com>
2021-08-27 19:46:36 -0300

    Create readme.d

---
+++ atmega328pb/eeprom/readme.d
@@ -0,0 +1,3 @@
+Classe que implementa a função de leitura e escrita da memória EEPROM interna do microcontrolador Atmega328pb. 
+Também contém um exemplo de uso da classe com operações simples de leitura e escrita.
+Essa classe não implementa as escritas e leituras em interrupções.
