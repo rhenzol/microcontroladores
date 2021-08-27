12cec5d6f095dab8337d955c1d62e00a0ffdac7f
rhenzol <54154565+rhenzol@users.noreply.github.com>
2021-08-27 19:49:51 -0300

    Update readme.d

--- atmega328pb/eeprom/readme.d
+++ atmega328pb/eeprom/readme.d
@@ -1,3 +1,3 @@
-Classe que implementa a função de leitura e escrita da memória EEPROM interna do microcontrolador Atmega328pb. 
-Também contém um exemplo de uso da classe com operações simples de leitura e escrita.
+Classe que implementa a função de leitura e escrita da memória EEPROM interna do microcontrolador Atmega328pb no arquivos "eeprom.h" e "eeprom.cpp". 
+Também contém um exemplo de uso da classe com operações simples de leitura e escrita no arquivo main.cpp.
 Essa classe não implementa as escritas e leituras em interrupções.
